69752bca8f9503ca718f06bcaec1e4a4e15fb45e
Bruno777202 <b.medeiros@edu.senai.br>
2026-05-06 17:06:23 -0300

    atavidade html/java

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
 // ATIVIDADE 01
 const ligar = () => {
+ let lampada = document.getElementById("lampada");
+ 
+ if(lampada.src.includes("apagada.jpg")){
+    lampada.src = "img/acesa.jpg";
+ }else{
+    lampada.src = ("img/apagada.jpg")
+ }
 
 };
 // -----------------------------------------
 
 // ATIVIDADE 02
 const gerarImagem = () => {
+let quantas = document.getElementById("inQtdImg")
+let canvas = document.getElementById("canvas")
+canvas.innerHTML = ""
+
+if(qtdIimagem <= 0){
+    alert("O campo quantidade não pode estar mulo ou negativo")
+               canvas.innerHTML += '<img id="logo" src="img/logo.png" alt="Logotipo SENAI"/>'
+
+}
 
 };
 // -----------------------------------------
